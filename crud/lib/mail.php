f8ca1137c44b22173a4dd93fb044cb2b99910806
TI01 <ti01@multimoveis.local>
2024-01-03 14:19:17 -0300

    email and upload

---
+++ crud/lib/mail.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require '/../vendor/autoload.php';
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 1;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPAuth = true;
+$mail->Username = 'carloseduardoorso1@gmail.com';
+$mail->Password = 'fuvd vmzt fdon jedm';
+
+$mail->SMTPSecure = '**tls**';
+$mail->isHTML(true);
+$mail->CharSet = 'UTF-8';
+
+$mail->setFrom('carloseduardoorso1@gmail.com', 'Carlos Orso');
+$mail->addAddress('yavet26495@watrf.com');
+$mail->Subject = 'E-mail de teste';
+
+$mail->Body = '<h1>Email enviado com sucesso!</h1><p>Parabéns, deu tudo certo!</p>';
+
+if($mail->send()){
+    echo 'email enviado';
+}else{
+    echo 'falha ao enviar o email';
+}
+?>
--- upload/index.php
+++ upload/index.php
@@ -3,17 +3,30 @@
 include('connection.php');
 
 
-if(isset($_FILES['arquivo'])){
-    $arquivo = $_FILES['arquivo'];
+if(isset($_GET['deletar'])){
+    $id = intval($_GET['deletar']);
+    $sql = $mysqli->query("SELECT * FROM files WHERE id = '$id'") or die($mysqli->error);
+    $arquivo = $sql->fetch_assoc();
 
-    if($arquivo['error']){
+    if(unlink($arquivo['path'])){
+        $deu_certo = $mysqli->query("DELETE FROM files WHERE id = '$id'") or die($mysqli->error);
+        if($deu_certo){
+            echo "<p> Arquivo excluido com sucesso!</p>";
+            
+        }
+    }
+
+}
+function enviarArquivo($error, $size, $name, $tmp_name){
+    include('connection.php');
+    if($error){
         die('Falha ao enviar o arquivo');
     }
-    if($arquivo['size'] > 2097152){
+    if($size > 2097152){
         die('Arquivo muito grande, máximo: 2MB');
     }
     $folder = 'files/';
-    $fileName = $arquivo['name'];
+    $fileName = $name;
     $newNameFile = uniqid();
     $extension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
 
@@ -21,14 +34,32 @@ if(isset($_FILES['arquivo'])){
         die('Tipo de arquivo não aceito');
     }
     $path = $folder . $newNameFile . '.' . $extension;
-    $deu_certo = move_uploaded_file($arquivo['tmp_name'], $path);
+    $deu_certo = move_uploaded_file($tmp_name, $path);
     if($deu_certo){
-        $mysqli->query("INSERT INTO files (nome, path) VALUES('$fileName', '$path')");
-        echo "<p> Arquivo enviado com sucesso enviado com sucesso! Para acessa-lo, clique aqui: <a target=\"_blank\"href=\"files/$newNameFile.$extension\">Clique aqui</a></p> ";
+        $mysqli->query("INSERT INTO files (nome, path) VALUES('$fileName', '$path')") or die($mysqli->error);
+        return true;
     }else{
-        echo "Falha ao enviar o arquivo";
+        return false;
     }
 }
+
+
+if(isset($_FILES['arquivos'])){
+    $arquivos = $_FILES['arquivos'];
+    $tudo_certo = true;
+    foreach($arquivos['name'] as $index=> $arq){
+        $deu_certo = enviarArquivo($arquivos['error'][$index], $arquivos['size'][$index], $arquivos['name'][$index], $arquivos['tmp_name'][$index]);
+        if(!$deu_certo){
+            $tudo_certo = false;
+        }
+    }
+    if($tudo_certo){
+        echo "<p> Todos os arquivos foram enviados com sucesso! </p>";
+    }else{
+        echo "<p> Falha ao enviar um ou mais arquivos!</p>";
+    }
+}
+
 
 $sql_arquivos = $mysqli->query("SELECT * FROM files") or die($mysqli->error);
 ?>
@@ -45,23 +76,27 @@ $sql_arquivos = $mysqli->query("SELECT * FROM files") or die($mysqli->error);
     <form method="post" enctype="multipart/form-data" action="">
         <p>
             <label for="">Selecione o arquivo</label>
-            <input name="arquivo" type="file">
+            <input multiple name="arquivos[]" type="file">
         </p>
         <button type="submit">Enviar arquivo</button>
     </form>
 
-    <table>
+    <table border="1" cellpadding="10">
         <thead>
+            <th>Preview</th>
             <th>Arquivo</th>
             <th>Data de envio</th>
+            <th></th>
         </thead>
         <tbody>
         <?php
             while($arquivo = $sql_arquivos->fetch_assoc()) {
             ?>
             <tr>
-                <td><?php echo $arquivo['nome']; ?></td>
-                <td></td>
+                <td><img height="50" src="<?php echo $arquivo['path'];?>" alt=""></td>
+                <td><a href="<?php echo $arquivo['path'] ;?>"><?php echo $arquivo['nome']; ?></a></td>
+                <td><?php echo date("d/m/Y", strtotime($arquivo['data_upload']));?></td>
+                <td><a href="index.php?deletar=<?php echo $arquivo['id'];?>">Deletar</a></td>
             </tr>
             <?php
             }
